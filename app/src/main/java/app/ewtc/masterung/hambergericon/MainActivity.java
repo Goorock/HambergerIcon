9281df7ab24a7678f271f6aab4f7ece2a1767eb6
MasterUNG <phrombutr@gmail.com>
2017-08-20 23:45:14 +0700

    Project Success

--- app/src/main/java/app/ewtc/masterung/hambergericon/MainActivity.java
+++ app/src/main/java/app/ewtc/masterung/hambergericon/MainActivity.java
@@ -1,17 +1,29 @@
 package app.ewtc.masterung.hambergericon;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import app.ewtc.masterung.hambergericon.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initial View
+        initialView();
+
+        //Add Fragment to Activity
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
@@ -19,5 +31,40 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }
 
+    }
+
+    private void initialView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }   // initialView
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
